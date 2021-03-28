ce26e33b9c3a0fbc59ce2b7a2396500e99424d24
Robert Olofsson <robo@khelekore.org>
2021-03-28 16:44:09 +0200

    Add all flags.

--- src/main/java/org/khelekore/parjac2/java11/Flags.java
+++ src/main/java/org/khelekore/parjac2/java11/Flags.java
@@ -17,3 +17,31 @@ public class Flags {
     // This one does not exist in Opcodes.
     public static final int ACC_DEFAULT = Opcodes.ACC_DEPRECATED << 1;
 }
+
+/*
+    public static final int ACC_ABSTRACT = 1024;
+    public static final int ACC_ANNOTATION = 8192;
+    public static final int ACC_BRIDGE = 64;
+    public static final int ACC_DEPRECATED = 131072;
+    public static final int ACC_ENUM = 16384;
+    public static final int ACC_FINAL = 16;
+    public static final int ACC_INTERFACE = 512;
+    public static final int ACC_MANDATED = 32768;
+    public static final int ACC_MODULE = 32768;
+    public static final int ACC_NATIVE = 256;
+    public static final int ACC_OPEN = 32;
+    public static final int ACC_PRIVATE = 2;
+    public static final int ACC_PROTECTED = 4;
+    public static final int ACC_PUBLIC = 1;
+    public static final int ACC_RECORD = 65536;
+    public static final int ACC_STATIC = 8;
+    public static final int ACC_STATIC_PHASE = 64;
+    public static final int ACC_STRICT = 2048;
+    public static final int ACC_SUPER = 32;
+    public static final int ACC_SYNCHRONIZED = 32;
+    public static final int ACC_SYNTHETIC = 4096;
+    public static final int ACC_TRANSIENT = 128;
+    public static final int ACC_TRANSITIVE = 32;
+    public static final int ACC_VARARGS = 128;
+    public static final int ACC_VOLATILE = 64;
+*/
