9ede9cc9cd619e0e8d404d19b2309b1bd0b4b3a1
Robert Olofsson <robo@khelekore.org>
2023-08-11 16:49:25 +0200

    Ported test of constructor parsing from parjac 1 to parjac 2.

---
+++ src/test/java/org/khelekore/parjac2/javacompiler/TestConstructorParsing.java
@@ -0,0 +1,73 @@
+package org.khelekore.parjac2.javacompiler;
+
+import org.khelekore.parjac2.CompilerDiagnosticCollector;
+import org.khelekore.parjac2.parser.Grammar;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestConstructorParsing {
+
+    private Grammar g;
+    private CompilerDiagnosticCollector diagnostics;
+
+    @BeforeClass
+    public void createLRParser () {
+	g = TestParserHelper.getJavaGrammarFromFile ("ConstructorDeclaration", false);
+    }
+
+    @BeforeTest
+    public void createDiagnostics () {
+	diagnostics = new CompilerDiagnosticCollector ();
+    }
+
+    @Test
+    public void testSimpleConstructor () {
+	testSuccessfulParse ("Foo () {}");
+	testSuccessfulParse ("Foo () {super();}");
+	testSuccessfulParse ("Foo (int a) {super();}");
+	testSuccessfulParse ("Foo (int a) {super(a);}");
+	testSuccessfulParse ("Foo (int a) {this();}");
+	testSuccessfulParse ("Foo (int a) {this(a);}");
+    }
+
+    @Test
+    public void testAnnotatedConstructor () {
+	testSuccessfulParse ("@Bar Foo () {}");
+	testSuccessfulParse ("@Bar @Baz Foo () {}");
+    }
+
+    @Test
+    public void testModifiersConstructor () {
+	testSuccessfulParse ("public Foo () {}");
+	testSuccessfulParse ("protected Foo () {}");
+	testSuccessfulParse ("private Foo () {}");
+
+	testSuccessfulParse ("@Bar public Foo () {}");
+	testSuccessfulParse ("@Bar protected Foo () {}");
+	testSuccessfulParse ("@Bar private Foo () {}");
+    }
+
+    @Test
+    public void testConstructorThatThrows () {
+	testSuccessfulParse ("Foo () throws A {}");
+	testSuccessfulParse ("Foo () throws A, B {}");
+    }
+
+    @Test
+    public void testGenericConstructor () {
+	testSuccessfulParse ("<T> Foo () {}");
+	testSuccessfulParse ("<T> Foo (T t) {}");
+	testSuccessfulParse ("<K, V> Foo () {}");
+    }
+
+    @Test
+    public void testGenericInvocations () {
+	testSuccessfulParse ("Foo () {<T>this();}");
+	testSuccessfulParse ("Foo () {<T>super();}");
+    }
+
+    private void testSuccessfulParse (String s) {
+	TestParserHelper.testSuccessfulParse (g, s, diagnostics, null);
+    }
+}
