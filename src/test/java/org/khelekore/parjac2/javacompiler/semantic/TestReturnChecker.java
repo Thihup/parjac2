a48b7744900c08e84cb01c4d771ec076b6e6971e
Robert Olofsson <robo@khelekore.org>
2023-12-15 11:39:42 +0100

    Add test for if with return: it should not mark what comes after as unreachable.

--- src/test/java/org/khelekore/parjac2/javacompiler/semantic/TestReturnChecker.java
+++ src/test/java/org/khelekore/parjac2/javacompiler/semantic/TestReturnChecker.java
@@ -8,4 +8,9 @@ public class TestReturnChecker extends TestCompilationErrorHandling {
     public void testUnreachableCode () {
 	testClass ("C.java", "class C { void a () { int x = 0; return; x++; }}", 1);
     }
+
+    @Test
+    public void testIfWithReturnDoesNotMarkUnreachable () {
+	testClass ("C.java", "class C { void a (int x) { if (x == 0) return; x++; }}", 0);
+    }
 }
