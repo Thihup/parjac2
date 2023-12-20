818b0978d70c8ca1e1fd3d3f58950223dce667d0
Robert Olofsson <robo@khelekore.org>
2023-12-20 13:45:12 +0100

    Rework try/catch/finally-handling so that the exception tables span the correct amount of code elements.

--- src/main/java/org/khelekore/parjac2/javacompiler/code/TryGenerator.java
+++ src/main/java/org/khelekore/parjac2/javacompiler/code/TryGenerator.java
@@ -11,39 +11,50 @@ import org.khelekore.parjac2.javacompiler.syntaxtree.TryStatement;
 import org.khelekore.parjac2.parsetree.ParseTreeNode;
 
 import io.github.dmlloyd.classfile.CodeBuilder;
+import io.github.dmlloyd.classfile.Label;
 import io.github.dmlloyd.classfile.TypeKind;
 
 public class TryGenerator {
     public static void handleTryStatement (MethodContentGenerator mcg, CodeBuilder cb, TryStatement t) {
+	// We would love to use cb.trying, but it does not deal with finally-blocks well.
+	// We get Exception tables that include all code in the finally-block.
+	/*
 	cb.trying (c -> handleBlockAndFinally (mcg, c, t.block (), t.finallyBlock ()),
 		   c -> handleCatches (mcg, c, t.catches (), t.finallyBlock ()));
+	*/
+	Label afterTry = cb.newLabel ();
+	Label tryStart = cb.newBoundLabel ();
+	mcg.handleStatements (cb, t.block ());
+	Label tryEnd = cb.newBoundLabel ();
+	Finally f = t.finallyBlock ();
+	if (f != null)
+	    mcg.handleStatements (cb, f);
+	cb.goto_ (afterTry);
 
-    }
-
-    private static void handleCatches (MethodContentGenerator mcg, CodeBuilder.CatchBuilder cb, Catches catches, Finally finallyBlock) {
-	if (catches != null) {
-	    for (ParseTreeNode p : catches.get ()) {
-		CatchClause cc = (CatchClause)p;
+	Catches cc = t.catches ();
+	if (cc != null) {
+	    for (ParseTreeNode p : cc.get ()) {
+		Label handler = cb.newBoundLabel ();
+		CatchClause c = (CatchClause)p;
 		ClassDesc exceptionType = null;
-		cb.catching (exceptionType, c -> mcg.handleStatements (c, cc.block ()));
+		mcg.handleStatements (cb, c.block ());
+		cb.exceptionCatch (tryStart, tryEnd, handler, exceptionType);
+		// TODO: probably inline finally-block here
+		cb.goto_ (afterTry);
 	    }
 	}
-	if (finallyBlock != null) {
-	    cb.catchingAll (c -> handleFinally (mcg, c, finallyBlock));
+
+	if (f != null) {
+	    Label handler = cb.newBoundLabel ();
+	    int exceptionSlot = cb.allocateLocal (TypeKind.ReferenceType);
+	    cb.astore (exceptionSlot);
+	    mcg.handleStatements (cb, f);
+	    cb.aload (exceptionSlot);
+	    cb.athrow ();
+	    cb.exceptionCatchAll (tryStart, tryEnd, handler);
 	}
+
+	cb.labelBinding (afterTry);
     }
 
-    private static void handleBlockAndFinally (MethodContentGenerator mcg, CodeBuilder cb, Block block, Finally finallyBlock) {
-	mcg.handleStatements (cb, block);
-	if (finallyBlock != null)
-	    mcg.handleStatements (cb, finallyBlock.block ());
-    }
-
-    private static void handleFinally (MethodContentGenerator mcg, CodeBuilder cb, Finally finallyBlock) {
-	int exceptionSlot = cb.allocateLocal (TypeKind.ReferenceType);
-	cb.astore (exceptionSlot);
-	mcg.handleStatements (cb, finallyBlock);
-	cb.aload (exceptionSlot);
-	cb.athrow ();
-    }
 }
