7db5a356128d4ce7b92879769dfeb29d451b202d
Robert Olofsson <robo@khelekore.org>
2024-02-05 16:05:56 +0100

    Added tests for catch handling.

--- src/test/java/org/khelekore/parjac2/javacompiler/compile/TestTryResources.java
+++ src/test/java/org/khelekore/parjac2/javacompiler/compile/TestTryResources.java
@@ -14,11 +14,33 @@ public class TestTryResources extends CompileAndRun {
 	assert e.closed : "Did not run close()";
     }
 
+    @Test
+    public void testCatchHandling () throws ReflectiveOperationException {
+	Method m = getMethod ("C", "public class C { public static void f (Runnable r, Runnable iaec) { " +
+			      "try { r.run (); } catch (IllegalArgumentException e) { iaec.run (); }}}",
+			      "f", Runnable.class, Runnable.class);
+	Runnable r1 = () -> {/* empty */};
+	Runnable r2 = () -> { throw new IllegalArgumentException (); };
+	ExceptionCatcher ec = new ExceptionCatcher ();
+	m.invoke (null, r1, ec);
+	assert !ec.gotIt;
+	m.invoke (null, r2, ec);
+	assert ec.gotIt;
+    }
+
     private static class TestAutoCloseable implements AutoCloseable {
 	public boolean closed = false;
 
 	@Override public void close () {
 	    closed = true;
+	}
+    }
+
+    private static class ExceptionCatcher implements Runnable {
+	boolean gotIt = false;
+
+	@Override public void run () {
+	    gotIt = true;
 	}
     }
 }
